0e41c45058143c6d5229f2f5a5b286c5f9bf0964
ZepmanBC <zepman@gmail.com>
2019-04-24 23:13:01 +0200

    ajout des static files pour AWS

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -179,7 +179,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
-if os.environ.get('ENV') == 'PRODUCTION':
+if os.environ.get('ENV') in ['PRODUCTION', 'AWS'] :
 
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
